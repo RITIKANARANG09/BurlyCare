5499bebc7cbb6d43df2a6e281cab25a7c4fb9f2e
ritika_narang <ritsnarang09@gmail.com>
2023-05-12 11:36:11 +0530

    initial

---
+++ index.js
@@ -0,0 +1,25 @@
+var url = 'form.html';
+function fun1(){
+window.location = url;
+};
+function fun2()
+{
+    alert("Call us on 0876-123-4567");
+}
+function fun3()
+{
+    window.location="//burlycare.com/#scrollToAppointment";
+}
+function fun4()
+{
+    alert(" You can visit our clinic to know more");
+}
+function d1(){window.location ="https://burlycare.com/anal-fissure"}
+function d2(){window.location ="https://burlycare.com/hernia"}
+function d3(){window.location ="https://burlycare.com/piles"}
+function d4(){window.location ="https://burlycare.com/anal-fissure"}
+function d5(){window.location ="https://burlycare.com/anal-fissure"}
+function size(){
+    var ht=document.getElementsByClassName("a").style.height;
+    ht++;
+}
